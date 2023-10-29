b5dd8d47b52e0f68b99730aab4f251064dc84522
nvvinh123 <nvvinh.dhkl17a3hn@sv.uneti.edu.vn>
2023-10-29 21:05:11 +0700

    bai tap c5

---
+++ bài 2/mmm.py
@@ -0,0 +1,12 @@
+x=10
+y=5
+tong=x+y
+hieu=x-y
+thuong=x/y
+tich=x*y
+
+print('ket qua cac phep tinh:')
+print("tong cua   ",x, '+' , y, '=' ,tong)
+print("hieu cua   ",x, '-' , y, '=' ,hieu)
+print("tich cua   ",x, '*' , y, '=' ,tich)
+print("thuong cua ",x, '/' , y, '=' ,thuong)
---
+++ bài 3/bài 3.py
@@ -0,0 +1,17 @@
+ten_hang = "sữa hộp vinamlik"
+so_luong = 5
+don_gia = 25000 
+so_tien_phai_tra = so_luong * don_gia
+print("Tên hàng:", ten_hang)
+print("Số lượng:", so_luong)
+print("Đơn giá:", don_gia)
+print("Tiền phải trả:",so_tien_phai_tra, "vnd")
+
+
+
+
+
+
+
+
+
